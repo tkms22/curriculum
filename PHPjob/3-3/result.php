0a5925b1ac957d48d1b5185e6e922755f22f9466
kido <mkidoch@gmail.com>
2023-07-08 19:16:18 +0900

    task

---
+++ PHPjob/3-3/result.php
@@ -0,0 +1,32 @@
+<?php
+$my_name = $_POST["my_name"];
+$number = $_POST['number'];
+$number *= mt_rand(1, 6);
+
+function getMultiplenumber($number) {
+    
+    if($number >= 37) {
+        echo "残念";
+    }
+    if(26 <= $number && $number < 37) {
+        return "大吉";
+    }
+    if(21 <= $number && $number < 26) {
+        return "吉";
+    }
+    if(16 <= $number && $number < 21) {
+        return "中吉";
+    }
+    if(11 <= $number && $number < 16) {
+        return "小吉";
+    }
+    if(1 <= $number && $number < 11) {
+        return "凶";
+    }
+}
+?>
+
+<?php echo date("Y-m-d H:i:s", time()); ?>
+<p>名前は<?php echo $my_name; ?>です。</p>
+<p>番号は<?php echo $number; ?>です。</p>
+<p>結果は<?php echo getMultiplenumber($number); ?>です。</p>
