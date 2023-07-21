9bad95eb3d875c8415900b58b8d870ba8cd09c34
kido <mkidoch@gmail.com>
2023-07-21 16:21:16 +0900

    修正

--- PHPjob/4-1/bugTest/test2/dbconnect.php
+++ PHPjob/4-1/bugTest/test2/dbconnect.php
@@ -1,12 +1,4 @@
 <?php
-define('DB_DATABASE', 'yigroupblog');
-// MySQLのユーザー名
-define('DB_USERNAME', 'root');
-// MySQLのログインパスワード
-define('DB_PASSWORD', "");
-// DSN
-define('PDO_DSN', 'mysql:host=localhost;charset=utf8;dbname='.DB_DATABASE);
-// セッション開始
 session_start();
 $db['host'] = "localhost";
 // ユーザー名
--- PHPjob/4-1/bugTest/test2/index.php
+++ PHPjob/4-1/bugTest/test2/index.php
@@ -26,12 +26,9 @@ if (isset($_POST["signUp"])) {
         // 3. エラー処理
         try {
             $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-            $stmt = $pdo->prepare("INSERT INTO users (name, password) VALUES (:username, :password)");
-            $stmt->bindParam(':username', $username);
-            $stmt->bindParam(':password', $password);
-            $stmt->execute();  // パスワードのハッシュ化を行う（今回は文字列のみなのでbindValue(変数の内容が変わらない)を使用せず、直接excuteに渡しても問題ない）
+            $stmt = $pdo->prepare("INSERT INTO users (name, password) VALUES (?, ?)");
+            $stmt->execute(array($username, password_hash($password, PASSWORD_DEFAULT)));  // パスワードのハッシュ化を行う（今回は文字列のみなのでbindValue(変数の内容が変わらない)を使用せず、直接excuteに渡しても問題ない）
             $userid = $pdo->lastinsertid();  // 登録した(DB側でauto_incrementした)IDを$useridに入れる
-
             $signUpMessage = '登録が完了しました。あなたの登録IDは ' . $userid . ' です。パスワードは ' . $password . ' です。';  // ログイン時に使用するIDとパスワード
         } catch (PDOException $e) {
             $errorMessage = 'データベースエラー';
