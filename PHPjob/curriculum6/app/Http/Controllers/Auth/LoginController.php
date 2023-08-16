7dbef0eed374d6c602a3aad043aa4947b6743477
kido <mkidoch@gmail.com>
2023-08-16 14:01:07 +0900

    logout実装

--- PHPjob/curriculum6/app/Http/Controllers/Auth/LoginController.php
+++ PHPjob/curriculum6/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -40,8 +41,6 @@ class LoginController extends Controller
     
     public function logout(Request $request)
     {
-        Auth::logout();
-
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
