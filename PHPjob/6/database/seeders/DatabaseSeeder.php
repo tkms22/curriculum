701024c97b31be1e476a25f11bcbaf704abef907
kido <mkidoch@gmail.com>
2023-08-30 17:34:18 +0900

    seeder実装

--- PHPjob/6/database/seeders/DatabaseSeeder.php
+++ PHPjob/6/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeders\SnsSeeder;
+use Database\Seeders\SnsSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
--- PHPjob/6/database/seeders/SnsSeeder.php
+++ PHPjob/6/database/seeders/SnsSeeder.php
@@ -18,19 +18,19 @@ class SnsSeeder extends Seeder
     {
         Post::create([
             'id' => '1',
-            'user_id' => '1',
+            'user_id' => '0001',
             'body' => 'hello',
         ]);
 
         Post::create([
             'id' => '2',
-            'user_id' => '2',
+            'user_id' => '0002',
             'body' => 'こんにちは',
         ]);
 
         Post::create([
             'id' => '3',
-            'user_id' => '3',
+            'user_id' => '0003',
             'body' => 'さよなら',
         ]);
     }
